680c91ddd811f704de1d0a8537d354e1e8e26d20
Kimkiheon <cbj2741@gmail.com>
2022-10-25 14:43:21 +0900

    jdbcTemplate 적용

--- src/main/java/dao/UserDao.java
+++ src/main/java/dao/UserDao.java
@@ -2,6 +2,8 @@ package dao;
 
 import domain.User;
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 
 import javax.sql.DataSource;
 import java.sql.Connection;
@@ -11,58 +13,34 @@ import java.sql.SQLException;
 import java.util.Map;
 
 public class UserDao {
-    private ConnectionMaker connectionMaker;
-    private Connection c;
-    private PreparedStatement ps;
-    private final DataSource dataSource;
-    private final JdbcContext jdbcContext;
+    private JdbcTemplate jdbcTemplate;
 
-
-    public UserDao(DataSource dataSource){
-        this.dataSource=dataSource;
-        this.jdbcContext=new JdbcContext(dataSource);
+    public UserDao(DataSource dataSource) {
+        this.jdbcTemplate = new JdbcTemplate(dataSource);
     }
 
-    public void jdbcContextWithStatementStrategy(StatementStrategy st) throws SQLException {
-        c=dataSource.getConnection();
-        ps=st.makePreparedStatement(c);
-        ps.executeUpdate();
-        ps.close();
-        c.close();
+    public void add(User user){
+        this.jdbcTemplate.update("insert into users (id, name, pasword) values (?,?,?);",
+                user.getId(), user.getName(), user.getPassword());
     }
 
-    public void add(User user) throws SQLException {
-        jdbcContext.workWithStatementStrategy(new StatementStrategy() {
+    public void deleteAll(){
+        this.jdbcTemplate.update("delete from users");
+    }
+
+    public int getCount(){
+        return this.jdbcTemplate.queryForObject("select count)*)from users;",Integer.class);
+    }
+
+    public User findById(String id) {
+        String sql = "select *from users where id = ?";
+        RowMapper<User> rowMapper = new RowMapper<User>() {
             @Override
-            public PreparedStatement makePreparedStatement(Connection c) throws SQLException {
-                ps=c.prepareStatement("insert into users (id,name,password) values(?,?,?)");
-                ps.setString(1,user.getId());
-                ps.setString(2,user.getName());
-                ps.setString(3,user.getPassword());
-                return ps;
+            public User mapRow(ResultSet rs, int rowNum) throws SQLException {
+                User user = new User(rs.getString("id"), rs.getString("name"), rs.getString("password"));
+                return user;
             }
-        });
+        };
+        return this.jdbcTemplate.queryForObject(sql, rowMapper, id);
     }
-
-    public void deleteAll() throws SQLException {
-        jdbcContextWithStatementStrategy
-                (c->c.prepareStatement("delete from users"));
-    }
-
-    public User findById(String id) throws SQLException {
-        c = connectionMaker.connectionMaker();
-        ps = c.prepareStatement("select *from users where id = ?");
-        ps.setString(1, id);
-        ResultSet rs = ps.executeQuery();
-
-        rs.next();
-        User user = new User(rs.getString("id"),rs.getString("name"),rs.getString("password"));
-        rs.close();
-        ps.close();
-        c.close();
-        if(user == null) throw new EmptyResultDataAccessException(1);
-        return user;
-    }
-
-
 }
