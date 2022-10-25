22732891bc803de96db59e5de05da619cb87d01a
Kimkiheon <cbj2741@gmail.com>
2022-10-25 15:34:31 +0900

    getAll() , Test

--- src/main/java/dao/UserDao.java
+++ src/main/java/dao/UserDao.java
@@ -1,17 +1,14 @@
 package dao;
 
 import domain.User;
-import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
-
 import javax.sql.DataSource;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
+
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
-import java.util.Map;
+
 
 public class UserDao {
     private JdbcTemplate jdbcTemplate;
@@ -19,6 +16,13 @@ public class UserDao {
     public UserDao(DataSource dataSource) {
         this.jdbcTemplate = new JdbcTemplate(dataSource);
     }
+    RowMapper<User> rowMapper = new RowMapper<User>() {
+        @Override
+        public User mapRow(ResultSet rs, int rowNum) throws SQLException {
+            User user = new User(rs.getString("id"), rs.getString("name"), rs.getString("password"));
+            return user;
+        }
+    };
 
     public void add(User user){
         this.jdbcTemplate.update("insert into users (id, name, pasword) values (?,?,?);",
@@ -34,26 +38,12 @@ public class UserDao {
     }
 
     public User findById(String id) {
-        String sql = "select *from users where id = ?";
-        RowMapper<User> rowMapper = new RowMapper<User>() {
-            @Override
-            public User mapRow(ResultSet rs, int rowNum) throws SQLException {
-                User user = new User(rs.getString("id"), rs.getString("name"), rs.getString("password"));
-                return user;
-            }
-        };
-        return this.jdbcTemplate.queryForObject(sql, rowMapper, id);
+        String sql = "SELECT * FROM users WHERE id = ?";
+        return jdbcTemplate.queryForObject(sql, rowMapper, id);
     }
 
     public List<User> getAll(){
         String sql = "select *from users order by id";
-        RowMapper<User> rowMapper = new RowMapper<User>() {
-            @Override
-            public User mapRow(ResultSet rs, int rowNum) throws SQLException {
-                User user = new User(rs.getString("id"),rs.getString("name"),rs.getString("password"));
-                return user;
-            }
-        };
         return this.jdbcTemplate.query(sql, rowMapper);
     }
 }
--- src/test/java/dao/UserDaoTest.java
+++ src/test/java/dao/UserDaoTest.java
@@ -12,6 +12,7 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.sql.SQLException;
+import java.util.List;
 
 @ExtendWith(SpringExtension.class)
 @ContextConfiguration(classes = UserDaoFactory.class)
@@ -19,21 +20,22 @@ public class UserDaoTest {
     @Autowired
     ApplicationContext context;
     UserDao userDao;
+    User user1, user2, user3;
     @BeforeEach
     void setUp(){
         this.userDao = context.getBean("awsUserDao",UserDao.class);
+        user1 = new User("1","김기헌","1111");
+        user2 = new User("2","기헌","2222");
+        user3 = new User("3","Kiheon","3333");
     }
 
     @Test
     void addAndSelect() throws SQLException{
-
-        String id = "333";
-        User user = new User(id,"spring","1123");
-        userDao.add(user);
-
-        User selectedUser = userDao.findById(id);
-        Assertions.assertEquals("spring",selectedUser.getName());
-        Assertions.assertEquals("1123",selectedUser.getPassword());
         userDao.deleteAll();
+        Assertions.assertEquals(0,userDao.getCount());
+        userDao.add(user1);
+        userDao.add(user2);
+        userDao.add(user3);
+        Assertions.assertEquals(3,userDao.getCount());
     }
 }
