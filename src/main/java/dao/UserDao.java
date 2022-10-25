0cd59cc85bbc6fd4198e9fee2a4c12b02eb483dd
Kimkiheon <cbj2741@gmail.com>
2022-10-25 14:45:36 +0900

    getAll() 추가

--- src/main/java/dao/UserDao.java
+++ src/main/java/dao/UserDao.java
@@ -10,6 +10,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.List;
 import java.util.Map;
 
 public class UserDao {
@@ -42,5 +43,17 @@ public class UserDao {
             }
         };
         return this.jdbcTemplate.queryForObject(sql, rowMapper, id);
+    }
+
+    public List<User> getAll(){
+        String sql = "select *from users order by id";
+        RowMapper<User> rowMapper = new RowMapper<User>() {
+            @Override
+            public User mapRow(ResultSet rs, int rowNum) throws SQLException {
+                User user = new User(rs.getString("id"),rs.getString("name"),rs.getString("password"));
+                return user;
+            }
+        };
+        return this.jdbcTemplate.query(sql, rowMapper);
     }
 }
