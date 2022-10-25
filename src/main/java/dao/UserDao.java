8fffbd8d8a5e07d53126f301a009bed263808e56
Kimkiheon <cbj2741@gmail.com>
2022-10-25 13:11:07 +0900

    Strategy pattern 적용

--- src/main/java/dao/UserDao.java
+++ src/main/java/dao/UserDao.java
@@ -22,24 +22,24 @@ public class UserDao {
         this.connectionMaker = connectionMaker;
     }
 
-    public void add(User user) throws SQLException {
-        c = connectionMaker.connectionMaker();
-        ps = c.prepareStatement("insert into users(id, name, password) values (?,?,?)");
-        ps.setString(1, user.getName());
-        ps.setString(2, user.getId());
-        ps.setString(3, user.getPassword());
-
+    public void jdbcContextWithStatementStrategy(StatementStrategy st) throws SQLException {
+        Connection c = connectionMaker.connectionMaker();
+        PreparedStatement ps = st.makePreparedStatement(c);
         ps.executeUpdate();
         ps.close();
         c.close();
+    }
+
+    public void add(User user) throws SQLException {
+        jdbcContextWithStatementStrategy(new AddStrategy(user));
     }
 
     public User findById(String id) throws SQLException {
         c = connectionMaker.connectionMaker();
         ps = c.prepareStatement("select *from users where id = ?");
         ps.setString(1, id);
-
         ResultSet rs = ps.executeQuery();
+
         rs.next();
         User user = new User(rs.getString("id"),rs.getString("name"),rs.getString("password"));
         rs.close();
